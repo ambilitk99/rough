3061f9914c7df5394444bb8feaf66a109d2371f6
ambilitk99 <95338201+ambilitk99@users.noreply.github.com>
2022-05-05 13:00:01 +0530

    Add files via upload

---
+++ Anu/leap.sh
@@ -0,0 +1,35 @@
+#Shell script to test whether the given year is a leap year or not
+
+if [ $# -gt 1 ]
+then
+   echo " syntax is <$0> [<year>]"
+   exit 1
+fi
+if [ $# -ne 1 ]
+then
+   yr=`date +%Y`
+else
+   yr=$1
+fi
+d4=`expr $yr % 4` 
+d100=`expr $yr % 100` 
+d400=`expr $yr % 400` 
+if [ $d4 -eq 0 -a $d100 -ne 0 -o $d400 -eq 0 ]
+then
+  echo " $yr is leap year "
+else
+  echo " $yr is not leap"
+fi 
+ 
+
+
+<<comment
+
+ksb@ksb-OptiPlex-3060:~/fathima/nw/script$ sh leap.sh
+ 2022 is not leap
+ksb@ksb-OptiPlex-3060:~/fathima/nw/script$ sh leap.sh 2021
+ 2021 is not leap
+ksb@ksb-OptiPlex-3060:~/fathima/nw/script$ sh leap.sh 2020
+ 2020 is leap year 
+
+comment
