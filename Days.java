9c228decf877af9529855fb084717a7d5cf799db
Fatwima <95403608+Fatwima@users.noreply.github.com>
2022-05-14 20:51:21 +0530

    Update Days.java

--- Days.java
+++ Days.java
@@ -79,3 +79,131 @@ else
          System.out.println("Specified year is not a leap year");
 }
 } 
+
+
+
+
+
+/*2. Create an interface having prototypes of functions area() and perimeter(). Create two classes Circle and Rectangle which implementsthe above interface. Create a menu driven program to find area and perimeter of objects.*/
+
+
+import java.util.*;
+interface total          //interface for total
+{
+    void input();
+    void area();
+    void perimeter();
+}
+
+class Circle implements total           //circle implements total
+{
+    int r=0;
+    double pi=3.14;
+    double area=0;
+    double perimeter=0;
+   
+    public void input()
+    {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter radius: ");
+        r=in.nextInt();
+    }
+   
+    public void area()
+    {
+        area=pi*r*r;
+        System.out.println("area of circle: " + area);
+    }
+   
+    public void perimeter()
+    {
+        perimeter=2*pi*r;
+        System.out.println("perimeter of circle: " + perimeter);
+    }
+}
+
+
+class Rectangle implements total                         //rectangle implements total
+{
+    int l=0,b=0;
+    double area=0;
+    double perimeter=0;
+   
+    public void input()
+    {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter length: ");
+        l=in.nextInt();
+        System.out.print("Enter breadth: ");
+        b=in.nextInt();
+    }
+   
+    public void area()
+    {
+        area=l*b;
+        System.out.println("area of rectangle: " + area);
+    }
+   
+    public void perimeter()
+    {
+        perimeter=2*(l+b);
+        System.out.println("perimeter of rectangle: " + perimeter);
+    }
+}
+
+
+class shape
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        Circle obj1 = new Circle();                //object for circle and rectangle
+        Rectangle obj2 = new Rectangle();
+       
+        while(true)
+        {
+            System.out.println("\n1.Circle\n2.Rectangle\n3.Exit\nEnter your choice\n\n");
+             int c=in.nextInt();
+            switch(c)
+            {
+                case 1: obj1.input();
+                    obj1.area();
+                    obj1.perimeter();
+                    break;
+                case 2: obj2.input();
+                    obj2.area();
+                    obj2.perimeter();
+                    break;
+                case 3:System.exit(0);
+                      default:System.out.println("invalid");
+            }
+        }
+    }
+}
+
+
+/*
+______________________________output______________________________
+1.Circle
+2.Rectangle
+3.Exit
+Enter your choice
+1
+Enter radius: 2
+area of circle: 12.56
+perimeter of circle: 12.56
+1.Circle
+2.Rectangle
+3.Exit
+Enter your choice
+2
+Enter length: 3
+Enter breadth: 4
+area of rectangle: 12.0
+perimeter of rectangle: 14.0
+1.Circle
+2.Rectangle
+3.Exit
+Enter your choice
+3
+*/
