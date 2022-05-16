eef231cc92f0c22a8375317a6d1e21d5ca849f7e
ambilitk99 <95338201+ambilitk99@users.noreply.github.com>
2022-05-16 15:36:26 +0530

    Add files via upload

---
+++ Anu/prgm6.sh
@@ -0,0 +1,42 @@
+#6.Shell script to count the number of words and lines in a file
+
+
+if [ $# -ne 1 ]
+then 
+	echo "syntax error"
+	echo "< $0 filename >"
+	exit
+fi
+
+fname=$1
+ter=`tty`
+exec < $fname
+i=0
+j=0
+while read line
+do
+	i=`expr $i + 1`
+
+for c in $line 
+do
+	j=`expr $j + 1`
+
+done
+
+done
+echo "$fname has"
+echo "no of lines->$i"
+echo "no of words->$j"
+exec < $ter
+
+
+<<com
+
+OUTPUT
+
+ksb@ksb-Lenovo-Product:~/ambili/NTWRK/Experiments$ sh prgm6.sh prgm2.sh
+prgm2.sh has
+no of lines->55
+no of words->150
+
+com
